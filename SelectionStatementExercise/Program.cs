9b0e6acdd7aa1cb849d11601f5e546b33ac96c1a
Royroy205 <leroyjr2344@live.com>
2023-08-05 11:57:40 -0500

    redo exercise

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -5,35 +5,41 @@
         static void Main(string[] args)
         {
             var r = new Random();
-            int FavNum = r.Next(1,10);
+            int FavNum = r.Next(1, 10);
             int response = 6;
-            
-          while (response != FavNum)
-            Console.Write("enter a number: ");
 
-            response = int.Parse(Console.ReadLine());
-
-            if (response < FavNum)
-
+            while (response != FavNum)
             {
-                Console.WriteLine($"Too low!/myour guess: {response}");
-            }
-            else if (response > FavNum) 
-            
-            {
-                Console.WriteLine($"Too High!/myour guess: {response}");
+                Console.Write("enter a number: ");
+                response = int.Parse(Console.ReadLine());
 
 
-            }
-            else 
-            {
-                Console.WriteLine($"Correct! {response} was my favorite number .");
-                    
-                    
-                    
-            }
-            
 
+
+
+
+
+                if (response < FavNum)
+
+                {
+                    Console.WriteLine($"Too low!/your guess: {response}");
+                }
+                else if (response > FavNum)
+
+                {
+                    Console.WriteLine($"Too high!/your guess: {response}");
+
+
+                }
+                else
+                {
+                    Console.WriteLine($"Correct! {response} was my favorite number .");
+
+
+
+                }
+
+            }
 
 
         }
